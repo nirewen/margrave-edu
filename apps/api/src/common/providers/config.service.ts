934c088286b6e24eee79776d9a8003d857bb66b9
nirewen <londero.edu@gmail.com>
2022-12-18 19:24:14 +0000

    feat: :sparkles: config service

---
+++ apps/api/src/common/providers/config.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common'
+import { ConfigService as NestConfig, Path, PathValue } from '@nestjs/config'
+import { Config } from 'src/config/config.interface'
+
+@Injectable()
+export class ConfigService<K = Config> extends NestConfig<K> {
+    public override get<P extends Path<K>>(path: P): PathValue<K, P> {
+        const value = super.get(path, { infer: true })
+
+        if (value === undefined) {
+            throw new Error(`NotFoundConfig: ${path}`)
+        }
+
+        return value
+    }
+}
