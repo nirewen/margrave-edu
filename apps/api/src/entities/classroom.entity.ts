b58975d35721748fbf2c1a10766d1923375381a5
nirewen <londero.edu@gmail.com>
2022-12-21 02:01:20 +0000

    feat: classroom entity

---
+++ apps/api/src/entities/classroom.entity.ts
@@ -0,0 +1,27 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm'
+import { Class } from './class.entity'
+
+export enum ClassroomType {
+    REGULAR = 'REGULAR',
+    COMPUTER = 'COMPUTER',
+    LABORATORY = 'LABORATORY',
+    GYMNASIUM = 'GYMNASIUM',
+}
+
+@Entity({ name: 'classrooms' })
+export class Classroom {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    building: string
+
+    @Column()
+    capacity: number
+
+    @Column({ type: 'enum', enum: ClassroomType })
+    type: ClassroomType
+
+    @OneToMany(() => Class, entity => entity.classroom)
+    classes: Class[]
+}
