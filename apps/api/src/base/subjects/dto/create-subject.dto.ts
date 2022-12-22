74f9444df8c953637c8668739850954b66b797c9
nirewen <londero.edu@gmail.com>
2022-12-22 00:16:03 +0000

    fix(subject): turn fields optional

--- apps/api/src/base/subjects/dto/create-subject.dto.ts
+++ apps/api/src/base/subjects/dto/create-subject.dto.ts
@@ -1,19 +1,23 @@
-import { IsHexColor, IsNumber, IsString, Min } from 'class-validator'
+import { IsHexColor, IsNumber, IsOptional, IsString, Min } from 'class-validator'
 
 export class CreateSubjectDTO {
     @IsString()
     name: string
 
     @IsHexColor()
+    @IsOptional()
     color: string
 
     @IsString()
+    @IsOptional()
     icon: string
 
     @IsString()
+    @IsOptional()
     type: string
 
     @IsNumber()
+    @IsOptional()
     @Min(20)
     hours: number
 
