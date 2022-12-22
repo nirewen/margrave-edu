a56a21b97a8f3bb37f47a22b51e92d08145c77fa
nirewen <londero.edu@gmail.com>
2022-12-22 21:39:57 +0000

    fix: use mapped-types

--- apps/api/src/base/assignments/dto/update-assignment.dto.ts
+++ apps/api/src/base/assignments/dto/update-assignment.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/swagger'
+import { PartialType } from '@nestjs/mapped-types'
 import { CreateAssignmentDTO } from './create-assignment.dto'
 
 export class UpdateAssignmentDTO extends PartialType(CreateAssignmentDTO) {}
