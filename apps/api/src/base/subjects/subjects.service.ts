044c843dbf4fe95c473195a4b0fc543bf1592084
nirewen <londero.edu@gmail.com>
2022-12-22 00:18:54 +0000

    fix(subject): provided user must be teacher

--- apps/api/src/base/subjects/subjects.service.ts
+++ apps/api/src/base/subjects/subjects.service.ts
@@ -1,6 +1,7 @@
 import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Subject } from 'src/entities/subject.entity'
+import { UserRole } from 'src/entities/user.entity'
 import { Repository } from 'typeorm'
 import { UsersService } from '../users/users.service'
 import { CreateSubjectDTO } from './dto/create-subject.dto'
@@ -16,6 +17,10 @@ export class SubjectsService {
     async create(createSubjectDto: CreateSubjectDTO) {
         const subject = this.subjects.create(createSubjectDto)
         const teacher = await this.usersService.findOneById(createSubjectDto.teacherId)
+
+        if (teacher.role !== UserRole.TEACHER) {
+            throw new BadRequestException('the provided user is not a TEACHER')
+        }
 
         subject.teacher = teacher
 
