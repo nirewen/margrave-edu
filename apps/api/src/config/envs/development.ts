5001cb564233ea817e7a3362fb587166a01f16bc
nirewen <londero.edu@gmail.com>
2022-12-22 14:27:45 +0000

    refactor(config): ditch DB_TYPE from env and use satisfies

--- apps/api/src/config/envs/development.ts
+++ apps/api/src/config/envs/development.ts
@@ -1,15 +1,18 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm'
+
 export const config = {
     db: {
-        type: process.env.DB_TYPE || 'postgres',
-        synchronize: false,
-        logging: true,
+        type: 'postgres',
         host: process.env.DB_HOST || '127.0.0.1',
         port: parseInt(process.env.DB_PORT, 10) || 5432,
         username: process.env.DB_USER || 'postgres',
         password: process.env.DB_PASSWORD || '1234',
         database: process.env.DB_NAME || 'margrave',
+
+        synchronize: false,
+        logging: true,
         autoLoadEntities: true,
-    },
+    } satisfies TypeOrmModuleOptions,
     storage: {
         url: process.env.SUPABASE_URL,
         key: process.env.SUPABASE_KEY,
