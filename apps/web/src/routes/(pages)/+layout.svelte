b1a73b29655d6a5d72a3c6f8149e6c666bb0be54
nirewen <londero.edu@gmail.com>
2022-12-29 00:33:16 +0000

    feat: add pages and routes

---
+++ apps/web/src/routes/(pages)/+layout.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import Menu from './components/Menu.svelte'
+</script>
+
+<main>
+    <Menu />
+    <div class="content">
+        <slot />
+    </div>
+</main>
+
+<style lang="scss">
+    main {
+        display: grid;
+        grid-template-areas:
+            'sidebar navbar'
+            'sidebar content';
+        grid-template-columns: 20rem auto;
+        grid-template-rows: 3rem auto;
+        gap: 1rem;
+        height: 100%;
+
+        > .content {
+            grid-area: content;
+        }
+    }
+</style>
---
+++ apps/web/src/routes/(pages)/components/Menu.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+    import { page } from '$app/stores'
+
+    const links = [
+        { name: 'Dashboard', route: '/dashboard', icon: '' },
+        { name: 'Estudantes', route: '/students', icon: '' },
+        { name: 'Professores', route: '/teachers', icon: '' },
+        { name: 'Aulas', route: '/lessons', icon: '' },
+        { name: 'Disciplinas', route: '/subjects', icon: '' },
+        { name: 'Turmas', route: '/classes', icon: '' },
+    ]
+</script>
+
+<nav>
+    <header>
+        <h1>Margrave</h1>
+    </header>
+    <p>Menu principal</p>
+    <ul>
+        {#each links as link}
+            <li class:active={$page.url.pathname.startsWith(link.route)}>
+                <a href={link.route}>{link.name}</a>
+            </li>
+        {/each}
+    </ul>
+</nav>
+
+<style lang="scss">
+    nav {
+        grid-area: sidebar;
+        padding: 2rem 1rem;
+
+        color: var(--accent-text);
+        background-color: var(--accent);
+
+        header {
+            margin-bottom: 1rem;
+            color: var(--gray-900);
+
+            > h1 {
+                font-size: 2rem;
+                font-weight: 700;
+            }
+        }
+
+        p {
+            font-size: 1.2rem;
+            font-weight: 600;
+            margin: 0.6rem 0;
+        }
+
+        ul {
+            display: flex;
+            flex-direction: column;
+            gap: 0.4rem;
+
+            li {
+                display: flex;
+                border-radius: 8px;
+                font-weight: 600;
+
+                a {
+                    flex: 1;
+                    padding: 0.4rem 0.6rem;
+                }
+
+                &.active {
+                    position: relative;
+                    color: var(--accent);
+                    background-color: var(--accent-text);
+                }
+            }
+        }
+    }
+</style>
