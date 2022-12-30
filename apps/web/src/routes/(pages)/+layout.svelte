9ffcc8290dff408a5f7df35c768ca6759b4915b5
nirewen <londero.edu@gmail.com>
2022-12-30 18:42:32 +0000

    style: better html tags

--- apps/web/src/routes/(pages)/+layout.svelte
+++ apps/web/src/routes/(pages)/+layout.svelte
@@ -3,29 +3,8 @@
     import NavBar from './components/NavBar.svelte'
 </script>
 
-<main>
-    <Menu />
-    <NavBar />
-    <div class="content">
-        <slot />
-    </div>
+<Menu />
+<NavBar />
+<main class="content">
+    <slot />
 </main>
-
-<style lang="scss">
-    main {
-        display: grid;
-        grid-template-areas:
-            'sidebar navbar'
-            'sidebar content';
-        grid-template-columns: 20rem auto;
-        grid-template-rows: 4rem auto;
-        height: 100%;
-
-        > .content {
-            grid-area: content;
-            overflow-y: auto;
-            padding: 0.8rem 1.4rem;
-            background-color: var(--content-bg);
-        }
-    }
-</style>
--- apps/web/src/routes/(pages)/dashboard/+page.svelte
+++ apps/web/src/routes/(pages)/dashboard/+page.svelte
@@ -9,63 +9,52 @@
 <svelte:head>
     <title>Dashboard | Margrave</title>
 </svelte:head>
-<main>
-    <header>
-        <h1>Dashboard</h1>
-        <h2>Bem vindo ao dashboard! Aqui você encontra estatísticas sobre o sistema.</h2>
-    </header>
-    <section class="stats">
-        <CountCard
-            href="/dashboard/students"
-            count={data.stats.students}
-            label={`estudante${p(data.stats.students)}`}
-        />
-        <CountCard
-            href="/dashboard/teachers"
-            count={data.stats.teachers}
-            label={`professor${p(data.stats.teachers, 'es')}`}
-        />
-        <CountCard
-            href="/dashboard/students"
-            count={data.stats.students}
-            label={`estudante${p(data.stats.students)}`}
-            style="grid-row: span 2;"
-        />
-        <CountCard
-            href="/dashboard/classes"
-            count={data.stats.lessons}
-            label={`turma${p(data.stats.lessons)}`}
-        />
-        <CountCard
-            href="/dashboard/lessons"
-            count={data.stats.subjects}
-            label={`aula${p(data.stats.subjects)}`}
-        />
-    </section>
-</main>
+
+<header>
+    <h1>Dashboard</h1>
+    <h2>Bem vindo ao dashboard! Aqui você encontra estatísticas sobre o sistema.</h2>
+</header>
+<section class="stats">
+    <CountCard
+        href="/dashboard/students"
+        count={data.stats.students}
+        label={`estudante${p(data.stats.students)}`}
+    />
+    <CountCard
+        href="/dashboard/teachers"
+        count={data.stats.teachers}
+        label={`professor${p(data.stats.teachers, 'es')}`}
+    />
+    <CountCard
+        href="/dashboard/students"
+        count={data.stats.students}
+        label={`estudante${p(data.stats.students)}`}
+        style="grid-row: span 2;"
+    />
+    <CountCard href="/dashboard/classes" count={data.stats.lessons} label={`turma${p(data.stats.lessons)}`} />
+    <CountCard
+        href="/dashboard/lessons"
+        count={data.stats.subjects}
+        label={`aula${p(data.stats.subjects)}`}
+    />
+</section>
 
 <style lang="scss">
-    main {
-        display: flex;
-        flex-direction: column;
+    header {
+        h1 {
+            font-size: 1.8rem;
+            font-weight: 700;
+        }
+
+        h2 {
+            color: var(--gray-400);
+        }
+    }
+
+    section.stats {
+        display: grid;
         gap: 1rem;
-
-        header {
-            h1 {
-                font-size: 1.8rem;
-                font-weight: 700;
-            }
-
-            h2 {
-                color: var(--gray-400);
-            }
-        }
-
-        section.stats {
-            display: grid;
-            gap: 1rem;
-            grid-template-columns: 1fr 1fr 2fr;
-            grid-template-rows: 1fr 1fr;
-        }
+        grid-template-columns: 1fr 1fr 2fr;
+        grid-template-rows: 1fr 1fr;
     }
 </style>
