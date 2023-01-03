5ea0ce28ca1323ab9a687632d028cecd8b51996a
nirewen <londero.edu@gmail.com>
2023-01-03 18:40:45 +0000

    feat: add error page

---
+++ apps/web/src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { page } from '$app/stores'
+</script>
+
+<img src="https://http.cat/{$page.status}.jpg" alt="" />
+
+<style lang="scss">
+    img {
+        grid-column: span 2;
+        grid-row: span 2;
+        place-self: center;
+    }
+</style>
--- apps/web/src/routes/admin/+layout.ts
+++ apps/web/src/routes/admin/+layout.ts
@@ -1,9 +1,17 @@
 import { loadWrapper as wrap } from '$lib/api'
 import type { User } from '$lib/types/User'
+import { error, redirect } from '@sveltejs/kit'
 import type { LayoutLoad } from './$types'
 
 export const load: LayoutLoad = wrap(async ({ api }) => {
     const user = await api.get<User>('/api/users/@me')
+
+    if (user.role !== 'ADMIN') {
+        // TODO: MOVER ISSO PRA HOOKS
+        throw error(403, {
+            message: '',
+        })
+    }
 
     return { user }
 })
