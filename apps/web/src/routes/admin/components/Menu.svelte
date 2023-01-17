06bc504c88ec3aeeeac8b06bc66f449930422950
nirewen <londero.edu@gmail.com>
2023-01-17 19:00:57 +0000

    style: collapsible menu

--- apps/web/src/routes/admin/components/Menu.svelte
+++ apps/web/src/routes/admin/components/Menu.svelte
@@ -2,6 +2,9 @@
     import MenuList from './MenuList.svelte'
 
     import { roles } from '$lib/util'
+    import { slide } from 'svelte/transition'
+    import { boolean } from '$lib/hooks/boolean'
+    import Button from '$lib/components/Button.svelte'
 
     const links = [
         { name: 'Dashboard', route: '/admin/dashboard', icon: 'ic:round-grid-view' },
@@ -19,28 +22,45 @@
         { name: 'Disciplinas', route: '/admin/subjects', icon: 'ic:round-menu-book' },
         { name: 'Turmas', route: '/admin/classes', icon: 'ic:baseline-groups-2' },
     ]
+
+    let open = boolean(true)
 </script>
 
-<nav>
+<nav class:open={$open}>
     <header>
+        <Button variant="ghost" icon on:click={open.toggle}>
+            <iconify-icon icon="ic:baseline-menu" width="2rem" />
+        </Button>
         <h1>Margrave</h1>
     </header>
-    <p>Menu principal</p>
-    <MenuList {links} />
+    {#if $open}
+        <p transition:slide>Menu principal</p>
+    {/if}
+    <MenuList open={$open} {links} />
 </nav>
 
 <style lang="scss">
     nav {
         grid-area: sidebar;
-        padding: 2rem 4rem 2rem 1rem;
+        padding: 1rem;
 
         color: var(--primary-text);
         background-color: var(--primary);
-        width: 20rem;
+        width: 5.5rem;
+        transition: width 200ms linear;
+        overflow: hidden;
+
+        &.open {
+            width: 20rem;
+        }
 
         header {
+            display: flex;
+            align-items: center;
+            gap: 1.5rem;
             margin-bottom: 1rem;
             color: var(--gray-900);
+            padding: 0.25rem;
 
             > h1 {
                 font-size: 2rem;
@@ -52,6 +72,7 @@
             font-size: 1.2rem;
             font-weight: 600;
             margin: 0.6rem 0.8rem;
+            white-space: nowrap;
         }
     }
 </style>
--- apps/web/src/routes/admin/components/MenuList.svelte
+++ apps/web/src/routes/admin/components/MenuList.svelte
@@ -1,22 +1,26 @@
 <script lang="ts">
     import { page } from '$app/stores'
+    import { slide } from 'svelte/transition'
 
     export let links: any[]
     export let child = false
+    export let open: boolean
 </script>
 
-<ul class:child>
+<ul class:child transition:slide>
     {#each links as { name, route, icon, routes }}
         <li class:nested={routes && routes.length > 0}>
-            <span
+            <a
                 class="label"
+                class:open
                 class:active={($page.url.pathname + '?' + $page.url.searchParams).startsWith(route)}
+                href={route}
             >
                 <iconify-icon {icon} width={28} />
-                <a href={route}>{name}</a>
-            </span>
-            {#if routes && routes.length > 0}
-                <svelte:self links={routes} child />
+                <span>{name}</span>
+            </a>
+            {#if routes && routes.length > 0 && open}
+                <svelte:self links={routes} child {open} />
             {/if}
         </li>
     {/each}
@@ -27,8 +31,8 @@
         display: flex;
         flex-direction: column;
         gap: 0.2rem;
-        width: 16rem;
         overflow-y: clip;
+        transition: all 200ms linear;
 
         li {
             display: flex;
@@ -38,15 +42,28 @@
             > .label {
                 display: flex;
                 align-items: center;
+                gap: 0.6rem;
                 padding: 0.4rem 0.8rem;
                 border-radius: 0.7rem;
                 font-weight: 600;
                 transition: all 200ms linear;
+                height: 50px;
+                max-width: 54px;
 
-                a {
+                > span {
                     flex: 1;
-                    padding: 0.4rem 0.6rem;
                     text-transform: capitalize;
+                    transition: all 200ms linear;
+                    max-width: 0;
+                    overflow: hidden;
+                }
+
+                &.open {
+                    max-width: 220px;
+
+                    > span {
+                        max-width: 300px;
+                    }
                 }
 
                 &.active {
