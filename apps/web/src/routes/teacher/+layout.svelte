91184fe7a272f7c4c527ef23a88f6359d1711449
nirewen <londero.edu@gmail.com>
2023-01-19 17:34:06 +0000

    feat: init teacher panel

--- apps/web/src/routes/teacher/+layout.svelte
+++ apps/web/src/routes/teacher/+layout.svelte
@@ -1,6 +1,19 @@
 <script lang="ts">
+    import { roles } from '$lib/util'
+
+    import Menu from '$lib/components/Menu.svelte'
+    import NavBar from '$lib/components/NavBar.svelte'
 </script>
 
+<Menu
+    links={[
+        { name: 'Dashboard', route: '/teacher/dashboard', icon: 'ic:round-grid-view' },
+        { name: 'Calendário', route: '/teacher/schedule', icon: 'ic:baseline-calendar-month' },
+        { name: 'Turmas', route: '/teacher/classes', icon: 'ic:baseline-groups-2' },
+        { name: 'Aulas', route: '/teacher/lessons', icon: 'ic:baseline-play-lesson' },
+    ]}
+/>
+<NavBar />
 <main class="content">
     <slot />
 </main>
