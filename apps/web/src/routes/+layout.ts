937e1404d138c14f9e13d8adff28f7f8b613e08a
nirewen <londero.edu@gmail.com>
2023-01-19 16:57:22 +0000

    feat: redirect user to correct panel

--- apps/web/src/routes/+layout.ts
+++ apps/web/src/routes/+layout.ts
@@ -1,9 +1,26 @@
+import { redirect } from '@sveltejs/kit'
+
 import { loadWrapper as wrap } from '$lib/api'
 import type { User } from '$lib/types/User'
+import type { Role } from '$lib/util'
 import type { LayoutLoad } from './$types'
 
-export const load: LayoutLoad = wrap(async ({ api }) => {
+const protectedRoutes: { route: string; roles: Role[] }[] = [
+    { route: '/admin', roles: ['ADMIN'] },
+    { route: '/teacher', roles: ['TEACHER'] },
+    { route: '/student', roles: ['STUDENT'] },
+]
+
+export const load: LayoutLoad = wrap(async ({ api, url }) => {
     const user = await api.get<User>('/api/users/@me').catch(() => null)
+    const route = protectedRoutes.find(r => url.pathname.startsWith(r.route))
+
+    // in case of protected route - redirect user to correct panel
+    if (user && route && !route.roles.includes(user.role)) {
+        url.pathname = url.pathname.replace(route.route, user.role.toLowerCase())
+
+        throw redirect(303, url.toString())
+    }
 
     return { user }
 })
