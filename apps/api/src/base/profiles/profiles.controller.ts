cc1afb5d1ffc3a1aabcab2ac73d47b9efd0453c2
nirewen <londero.edu@gmail.com>
2023-01-19 17:33:29 +0000

    feat: ability to update own profile

--- apps/api/src/base/profiles/profiles.controller.ts
+++ apps/api/src/base/profiles/profiles.controller.ts
@@ -1,13 +1,14 @@
 import { OrGuard } from '@nest-lab/or-guard'
 import { Body, Controller, Get, Param, Patch, UseGuards } from '@nestjs/common'
+import { Payload } from 'src/auth/auth.interface'
 import { CurrentUserGuard, RolesGuard } from 'src/auth/guards'
-import { Roles } from 'src/common/decorators'
+import { ReqUser, Roles } from 'src/common/decorators'
 import { UserRole } from 'src/entities/user.entity'
 import { UpdateProfileDTO } from './dto/update-profile.dto'
 import { ProfilesService } from './profiles.service'
 
 @Controller('profiles')
-@UseGuards(OrGuard([CurrentUserGuard, RolesGuard]))
+@UseGuards(RolesGuard)
 @Roles(UserRole.ADMIN)
 export class ProfilesController {
     constructor(private readonly profilesService: ProfilesService) {}
@@ -15,6 +16,16 @@ export class ProfilesController {
     @Get()
     findAll() {
         return this.profilesService.findAll()
+    }
+
+    @Get('@me')
+    me(@ReqUser() payload: Payload) {
+        return this.findOne(payload.id)
+    }
+
+    @Patch('@me')
+    edit(@ReqUser() payload: Payload, @Body() body: UpdateProfileDTO) {
+        return this.update(payload.id, body)
     }
 
     @Get(':id')
