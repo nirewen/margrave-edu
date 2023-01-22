555ce87f930cb726205f393ae456f0abe2d6a257
nirewen <londero.edu@gmail.com>
2023-01-22 20:26:52 +0000

    fix: wrong redirect status code

--- apps/web/src/routes/admin/users/[id]/edit/+page.server.ts
+++ apps/web/src/routes/admin/users/[id]/edit/+page.server.ts
@@ -50,11 +50,11 @@ export const actions: Actions = {
             avatar: obj.avatar.size > 0 ? await encodeBase64(obj.avatar) : null,
         })
 
-        throw redirect(301, `/admin/users`)
+        throw redirect(302, `/admin/users`)
     }),
     delete: wrap(async ({ request, api, params }) => {
         await api.delete(`/api/profiles/${params.id}`)
 
-        throw redirect(301, `/admin/users`)
+        throw redirect(302, `/admin/users`)
     }),
 }
